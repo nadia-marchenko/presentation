f0f6970dadca28bb1522d9b159f31bb8735bdfbe
Nadya Marchanka <nadia.besarab@gmail.com>
2020-05-10 16:15:49 +0300

    feat: add info no results

--- src/js/MoviesComponent.js
+++ src/js/MoviesComponent.js
@@ -24,54 +24,52 @@ export default class MoviesComponent {
 
     this.root.insertAdjacentHTML('beforeend', CARDS);
 
-    this.fetchMovies(`https://www.omdbapi.com/?s=${inputMovie}&apikey=e504ed78`, inputMovie);
+    this.fetchMovies(inputMovie);
 
     return this.root;
   }
 
-  async fetchMovies(url, inputMovie) {
-    // await Helper.fetchPost(url).then((content) => this.addMovies(content));
-    await Helper.fetchPost(url).then((content) => { this.addMovies(content, inputMovie), console.log(content)});
+  async fetchMovies(inputMovie) {
+    try {
+      const URL = `https://www.omdbapi.com/?s=${inputMovie}&apikey=e504ed78`;
+      await Helper.fetchPost(URL).then((content) => ((content.Response === 'False' && content.Error === 'Movie not found!')
+        ? this.root.insertAdjacentHTML('afterbegin', `<h6 class="no-results">No results for "${inputMovie}"</h6>`)
+        : this.addMovies(content)
+      ));
+    } catch (error) {
+      throw new Error('No data');
+    }
   }
 
-  addMovies(searchResult, inputMovie) {
+  addMovies(searchResult) {
     try {
-      // if (searchResult.Error === 'Too many results.') {
-      //   this.fetchMovies();
-      // }
+      this.root.querySelector('.swiper-wrapper').innerHTML = '';
       for (let i = 0; i < searchResult.Search.length; i += 1) {
         const card = new MovieCardComponent();
         this.root.querySelector('.swiper-wrapper').append(card.init(searchResult.Search[i]));
       }
       Helper.addSwiper();
     } catch (error) {
-      this.root.insertAdjacentHTML('afterbegin', `<h2 class="no-results">No results for ${searchResult}</h2>`);
       throw new Error('No data');
     }
   }
 
   changeMovies(inputMovie) {
-    let movie = inputMovie;
-    this.root.querySelector('.swiper-wrapper').innerHTML = '';
+    if (this.root.querySelector('h6')) {
+      this.root.querySelector('h6').remove();
+    }
     if (/[а-я]/.test(inputMovie)) {
       this.getTranslate(inputMovie);
     } else {
-      this.fetchMovies(`https://www.omdbapi.com/?s=${movie}&apikey=e504ed78`);
+      this.fetchMovies(inputMovie);
     }
     Helper.hideSpinner();
   }
-  // isRussianWord() {
-  //   if (/[а-я]/.test(inputMovie)) {
-  //     inputMovie = this.getTranslate();
-  //   }
-  // }
 
   async getTranslate(word) {
     const YANDEX_KEY = 'trnsl.1.1.20200509T171523Z.8afa2930a0dbf88e.b4b9b4b699c21bdf051b12aa18e705c7e16a2d21';
     const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${YANDEX_KEY}&text=${word}&lang=ru-en`;
     this.root.insertAdjacentHTML('afterbegin', `<h6>Showing results for "${word}"</h6>`);
-    await Helper.fetchPost(url).then((content) => 
-      this.fetchMovies(`https://www.omdbapi.com/?s=${content.text[0]}&apikey=e504ed78`)
-    );
+    await Helper.fetchPost(url).then((content) => this.fetchMovies(content.text[0]));
   }
 }
